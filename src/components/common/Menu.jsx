e5804402712e087fc8019f7e5ffb431a1523012e
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-09-06 08:41:53 -0300

    maquetado del navbar terminado

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -3,45 +3,35 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../../assets/logo.png";
 
 function Menu() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary border-bottom border-success border-opacity-75 shadow-sm"
+    >
+      <Container className="d-flex">
+        <Navbar.Brand href="#">
+          <img src={logo} alt="logo" height="55px" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+        <Navbar.Collapse>
+          <Nav className="text-center ms-auto my-2 my-lg-0">
+            <Nav.Link href="#action1">Inicio</Nav.Link>
+            <Nav.Link href="#action2">Productos</Nav.Link>
+            <Nav.Link href="#action2">Contacto</Nav.Link>
+            <Nav.Link href="#">Sobre Nosotros</Nav.Link>
+            <Nav.Link href="#">Iniciar Sesión</Nav.Link>
+            <Form className="d-flex ms-2">
+              <Form.Control
+                type="search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Buscar</Button>
+            </Form>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
