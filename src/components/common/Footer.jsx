b3aaf23a662b0de01b5543757b70931f375cc073
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-09-07 15:31:23 -0300

    creando nuevo footer, con maquetado distinto y color distinto

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,13 +4,51 @@ import { Col, Container, Row } from "react-bootstrap";
 const Footer = () => {
   return (
     <>
-      <div className="bg-dark">
-        <Container className="main text-uppercase py-5 text-light">
+      <div className="bg-body-tertiary border-top border-success border-opacity-75">
+        <Container className="main py-5 text-dark">
           <Row>
-            <Col>Contactanos</Col>
-            <Col>Horarios de atencion</Col>
-            <Col>Ayuda</Col>
-            <Col>Seguinos</Col>
+            <Col>
+              <h6 className="text-uppercase">Contactanos</h6>
+              <ul className="list-unstyled">
+                <li>3876423276 - Metán</li>
+                <li>3876488605 - WhatsApp - Metán</li>
+              </ul>
+              <ul className="list-unstyled">
+                <li>3876488605 - Rosario de la frontera</li>
+                <li>3876669121 - WhatsApp - Rosario de la Frontera</li>
+              </ul>
+            </Col>
+            <Col>
+              <h6 className="text-uppercase">Horarios de atención</h6>
+              <ul className="list-unstyled">
+                <li>Lunes a Sabados 8:30 a 13:00hs y de 17:30 a 21:30hs</li>
+              </ul>
+            </Col>
+            <Col>
+              <h6 className="text-uppercase">Ayuda</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className=" text-light">
+                    Contacto
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className=" text-light">
+                    Locales
+                  </a>
+                </li>
+              </ul>
+            </Col>
+            <Col>
+              <h6 className="text-uppercase">Seguinos</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className=" text-light">
+                    Facebook
+                  </a>
+                </li>
+              </ul>
+            </Col>
           </Row>
         </Container>
       </div>
