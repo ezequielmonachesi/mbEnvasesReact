e7d9ff4719d2e01050a9253dedfdc9680110bfb9
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-09-07 23:53:35 -0300

    seccion de cuatro cards de trayectoria terminado

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import {
+  BoxSeamFill,
+  HouseCheckFill,
+  PeopleFill,
+  RocketTakeoff,
+} from "react-bootstrap-icons";
 
 const Inicio = () => {
   return (
@@ -33,6 +39,68 @@ const Inicio = () => {
           </div>
         </div>
       </Container>
+      {/* Trayectoria */}
+      <Container>
+        <Row>
+          <Col md={5} lg={3}>
+            <div class="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
+              <span class="display-6 lh-1 text-blue mb-0">
+                <RocketTakeoff></RocketTakeoff>
+              </span>
+              <div class="ms-4 h6 fw-normal mb-0">
+                <div class="d-flex">
+                  <h5 class="mb-0 fw-bold">13</h5>
+                  <span class="mb-0 h5">+ años</span>
+                </div>
+                <p class="mb-0">Trayectoria</p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5} lg={3}>
+            <div class="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
+              <span class="display-6 lh-1 text-blue mb-0">
+                <HouseCheckFill></HouseCheckFill>
+              </span>
+              <div class="ms-4 h6 fw-normal mb-0">
+                <div class="d-flex">
+                  <h5 class="mb-0 fw-bold">2</h5>
+                  <span class="mb-0 h5">(Dos)</span>
+                </div>
+                <p class="mb-0">Sucursales</p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5} lg={3}>
+            <div class="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
+              <span class="display-6 lh-1 text-blue mb-0">
+                <PeopleFill></PeopleFill>
+              </span>
+              <div class="ms-4 h6 fw-normal mb-0">
+                <div class="d-flex">
+                  <h5 class="mb-0 fw-bold">500</h5>
+                  <span class="mb-0 h5">+</span>
+                </div>
+                <p class="mb-0">Clientes</p>
+              </div>
+            </div>
+          </Col>
+          <Col md={5} lg={3}>
+            <div class="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
+              <span class="display-6 lh-1 text-blue mb-0">
+                <BoxSeamFill></BoxSeamFill>
+              </span>
+              <div class="ms-4 h6 fw-normal mb-0">
+                <div class="d-flex">
+                  <h5 class="mb-0 fw-bold">1000</h5>
+                  <span class="mb-0 h5">+</span>
+                </div>
+                <p class="mb-0">Productos</p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      
     </>
   );
 };
