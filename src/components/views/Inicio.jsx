0ae3f25e9c251fa983c769934a326c1954976f4f
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-09-07 23:22:36 -0300

    banner principal terminado

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Inicio = () => {
-  return <Container className="mainSection"></Container>;
+  return (
+    <>
+      <Container
+        fluid
+        className="mainSection banner d-flex justify-content-center align-items-center"
+      >
+        <h1 className="display-1 text-white text-center">
+          Bienvenidos a envasesMB
+        </h1>
+      </Container>
+      <Container>
+        <div className="shadow text-center py-5 border-3 my-5">
+          <h2 class="">Nuestros productos</h2>
+          <h5 class="mt-3 text-secondary">
+            Papel - Aluminio - Cartón - Plástico - Telgopor
+          </h5>
+          <p class="my-4 text-secondary">
+            Contamos con diversa variedad de productos en Envases MB
+          </p>
+          <div>
+            <Button variant="success" className="mx-2">Consultanos</Button>
+            <Button variant="outline-success" className="mx-2">Sobre Nosotros</Button>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Inicio;
