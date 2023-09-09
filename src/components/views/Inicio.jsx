ab4eac52316c21e2f8cc6b70f69525614b25bc79
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-09-09 15:57:12 -0300

    se corrige nombre de className

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -22,11 +22,11 @@ const Inicio = () => {
       {/* Seccion Nuestros productos */}
       <Container>
         <div className="shadow text-center py-5 border-3 my-5">
-          <h2 class="">Nuestros productos</h2>
-          <h5 class="mt-3 text-secondary">
+          <h2>Nuestros productos</h2>
+          <h5 className="mt-3 text-secondary">
             Papel - Aluminio - Cartón - Plástico - Telgopor
           </h5>
-          <p class="my-4 text-secondary">
+          <p className="my-4 text-secondary">
             Contamos con diversa variedad de productos en Envases MB
           </p>
           <div>
@@ -43,64 +43,63 @@ const Inicio = () => {
       <Container>
         <Row>
           <Col md={5} lg={3}>
-            <div class="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
-              <span class="display-6 lh-1 text-blue mb-0">
+            <div className="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
+              <span className="display-6 lh-1 text-blue mb-0">
                 <RocketTakeoff></RocketTakeoff>
               </span>
-              <div class="ms-4 h6 fw-normal mb-0">
-                <div class="d-flex">
-                  <h5 class="mb-0 fw-bold">13</h5>
-                  <span class="mb-0 h5">+ años</span>
+              <div className="ms-4 h6 fw-normal mb-0">
+                <div className="d-flex">
+                  <h5 className="mb-0 fw-bold">13</h5>
+                  <span className="mb-0 h5">+ años</span>
                 </div>
-                <p class="mb-0">Trayectoria</p>
+                <p className="mb-0">Trayectoria</p>
               </div>
             </div>
           </Col>
           <Col md={5} lg={3}>
-            <div class="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
-              <span class="display-6 lh-1 text-blue mb-0">
+            <div className="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
+              <span className="display-6 lh-1 text-blue mb-0">
                 <HouseCheckFill></HouseCheckFill>
               </span>
-              <div class="ms-4 h6 fw-normal mb-0">
-                <div class="d-flex">
-                  <h5 class="mb-0 fw-bold">2</h5>
-                  <span class="mb-0 h5">(Dos)</span>
+              <div className="ms-4 h6 fw-normal mb-0">
+                <div className="d-flex">
+                  <h5 className="mb-0 fw-bold">2</h5>
+                  <span className="mb-0 h5">(Dos)</span>
                 </div>
-                <p class="mb-0">Sucursales</p>
+                <p className="mb-0">Sucursales</p>
               </div>
             </div>
           </Col>
           <Col md={5} lg={3}>
-            <div class="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
-              <span class="display-6 lh-1 text-blue mb-0">
+            <div className="d-flex justify-content-center align-items-center p-4 bg-success bg-opacity-50 rounded-3 shadow">
+              <span className="display-6 lh-1 text-blue mb-0">
                 <PeopleFill></PeopleFill>
               </span>
-              <div class="ms-4 h6 fw-normal mb-0">
-                <div class="d-flex">
-                  <h5 class="mb-0 fw-bold">500</h5>
-                  <span class="mb-0 h5">+</span>
+              <div className="ms-4 h6 fw-normal mb-0">
+                <div className="d-flex">
+                  <h5 className="mb-0 fw-bold">500</h5>
+                  <span className="mb-0 h5">+</span>
                 </div>
-                <p class="mb-0">Clientes</p>
+                <p className="mb-0">Clientes</p>
               </div>
             </div>
           </Col>
           <Col md={5} lg={3}>
-            <div class="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
-              <span class="display-6 lh-1 text-blue mb-0">
+            <div className="d-flex justify-content-center align-items-center p-4 bg-warning bg-opacity-50 rounded-3 shadow">
+              <span className="display-6 lh-1 text-blue mb-0">
                 <BoxSeamFill></BoxSeamFill>
               </span>
-              <div class="ms-4 h6 fw-normal mb-0">
-                <div class="d-flex">
-                  <h5 class="mb-0 fw-bold">1000</h5>
-                  <span class="mb-0 h5">+</span>
+              <div className="ms-4 h6 fw-normal mb-0">
+                <div className="d-flex">
+                  <h5 className="mb-0 fw-bold">1000</h5>
+                  <span className="mb-0 h5">+</span>
                 </div>
-                <p class="mb-0">Productos</p>
+                <p className="mb-0">Productos</p>
               </div>
             </div>
           </Col>
         </Row>
       </Container>
-      
     </>
   );
 };
